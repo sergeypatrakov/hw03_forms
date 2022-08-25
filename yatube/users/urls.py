39a723e3aa4671be00c7cea6e9b3eb67676b2a73
Sergey Patrakov <sergeypatrakov@MacBook-Pro-Sergey.local>
2022-08-25 17:49:52 +0300

    Done

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth.views import (
   PasswordResetConfirmView,
   PasswordResetDoneView,
   PasswordResetView,
-)  
+)
 from django.urls import path
 
 from . import views
@@ -16,47 +16,43 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path(
-      'logout/',
+    path('logout/',
       LogoutView.as_view(template_name='users/logged_out.html'),
       name='logout'
     ),
-    path(
-      'login/',
+    path('login/',
       LoginView.as_view(template_name='users/login.html'),
       name='login'
     ),
-    path(
-      'password_change/',
-      PasswordChangeView.as_view(template_name='users/password_change_form.html'),
+    path('password_change/',
+      PasswordChangeView.as_view(
+      template_name='users/password_change_form.html'),
       name='password_change'
     ),
-    path(
-      'password_change/done/',
-      PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+    path('password_change/done/',
+      PasswordChangeDoneView.as_view(
+      template_name='users/password_change_done.html'),
       name='password_change_done'
     ),
-    path(
-      'password_reset/',
-      PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+    path('password_reset/',
+      PasswordResetView.as_view(
+      template_name='users/password_reset_form.html'),
       name='password_reset',
     ),
-    path(
-      'password_reset/done/',
-      PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+    path('password_reset/done/',
+      PasswordResetDoneView.as_view(
+      template_name='users/password_reset_done.html'),
       name='password_reset_done',
     ),
-    path(
-      'reset/<uidb64>/<token>/',
+    path('reset/<uidb64>/<token>/',
       PasswordResetConfirmView.as_view(
-        template_name='users/password_reset_confirm.html'
+      template_name='users/password_reset_confirm.html'
       ),
       name='password_reset_confirm'
     ),
-    path(
-      'reset/done/',
+    path('reset/done/',
       PasswordResetCompleteView.as_view(
-        template_name='users/password_reset_complete.html'
+      template_name='users/password_reset_complete.html'
       ),
       name='password_reset_complete'
     ),
--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from django.views.generic import CreateView
 
 from django.urls import reverse_lazy
